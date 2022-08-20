17327b64cc78c3fcd5b63d26a7d6d1cb93412b88
vidal1101 <rodrigovidal278@gmail.com>
2022-08-19 23:22:13 -0600

    mapa con su posiciones y cambio de estilo listo, y marcadores

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -14,45 +14,60 @@ class _HomePageState extends State<HomePage> {
 
   Completer<GoogleMapController> _controller = Completer();
 
-   final CameraPosition puntoIncial = CameraPosition(
+  MapType maptype = MapType.normal;
+
+
+  final CameraPosition puntoIncial = CameraPosition(
     target: LatLng(10.628142, -85.432920),
     zoom: 17,
     tilt: 50, 
     bearing: 50, 
-    );
+  );
 
   @override
   Widget build(BuildContext context) {
 
-    Set<Marker> markers = new Set<Marker>();
+    Set<Marker> markers = Set<Marker>();
 
     markers.add(new Marker(
       markerId: MarkerId('geo-location'),
       position: LatLng(10.628142, -85.432920),
     ));
 
-
-
-    return Scaffold(
-      body: GoogleMap(
-        markers: markers,
-        compassEnabled: true,
-        mapType: MapType.normal,
-        initialCameraPosition: puntoIncial,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('Punto inicial'),
-        icon: Icon(Icons.directions_boat),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-   );
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Mapa'),
+          actions: [
+            IconButton(onPressed: (){
+              if(maptype == MapType.normal) maptype = MapType.satellite;
+              else maptype = MapType.normal;
+              setState(() {});
+            }, 
+            icon: Icon(Icons.map_outlined))
+          ],
+        ),
+        body: GoogleMap(
+          markers: markers,
+          mapType: maptype,
+          initialCameraPosition: puntoIncial,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: _goToTheLake,
+          label: Text('Punto inicial'),
+          icon: Icon(Icons.directions_boat),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+       ),
+    );
   }
+
   Future<void> _goToTheLake() async {
     final GoogleMapController controller = await _controller.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(puntoIncial));
   }
+  
 }
