26707fd625213efc7d50f9c1894eddff73a0dfbf
vidal1101 <rodrigovidal278@gmail.com>
2022-08-19 22:41:24 -0600

    cargando mapa

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -1,15 +1,35 @@
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import 'dart:async';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  Completer<GoogleMapController> _controller = Completer();
 
   @override
   Widget build(BuildContext context) {
+
+    final CameraPosition puntoIncial = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+    );
+
     return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
-     ),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: puntoIncial,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
    );
   }
 }
